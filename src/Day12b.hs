76c2a4ebaabde91d27a85e30334028a12bd123a5
Joel Burget <joelburget@gmail.com>
2016-04-23 11:55:16 -0400

    Push propagators implementation forward.

--- src/Day12b.hs
+++ src/Day12b.hs
@@ -2,8 +2,8 @@
 {-# LANGUAGE Rank2Types #-}
 module Day12b where
 
+import Control.Lens (Lens')
 import Control.Monad.ST
-import Data.Maybe (isJust)
 import Data.Propagator
 import Data.Propagator.Cell
 
@@ -15,62 +15,91 @@ data Term
   | FVar String (Maybe Type)
   | Abs (Maybe Term) (Maybe Type)
   | App (Maybe Term) (Maybe Term) (Maybe Type)
-  -- | Annot (TypeCell s) (TermCell s)
+  -- | Annot (Maybe Term) (Maybe Type)
   -- | Type
+  deriving Show
+
+-- orphans!
+instance Propagated a => Propagated (Maybe a) where
+  merge (Just a) (Just b) = Just <$> merge a b
+  merge a@(Just _) Nothing = Change False a
+  merge Nothing b@(Just _) = Change True b
+  merge n _ = Change False n
+
+instance Propagated String where
+
+instance Propagated Term where
+  merge (BVar i1 ty1) (BVar i2 ty2) =
+    BVar <$> merge i1 i2 <*> merge ty1 ty2
+  merge (FVar s1 ty1) (FVar s2 ty2) =
+    FVar <$> merge s1 s2 <*> merge ty1 ty2
+  merge (Abs t1 ty1) (Abs t2 ty2) =
+    Abs <$> merge t1 t2 <*> merge ty1 ty2
+  merge (App t11 t12 ty1) (App t21 t22 ty2) =
+    App <$> merge t11 t21 <*> merge t12 t22 <*> merge ty1 ty2
+  merge _ _ = Contradiction mempty "term merge"
+
+termTy :: Lens' Term (Maybe Type)
+termTy f tm = case tm of
+  BVar i ty -> BVar i <$> f ty
+  FVar s ty -> FVar s <$> f ty
+  Abs body ty -> Abs body <$> f ty
+  App t1 t2 ty -> App t1 t2 <$> f ty
+
 
 bVar :: Cell s Int -> Cell s Type -> Cell s Term -> ST s ()
-bVar iCell tyCell cell = do
+bVar iCell tyCell c = do
   watch iCell $ \iVal ->
     with tyCell $ \tyVal ->
-      write cell (BVar iVal (Just tyVal))
+      write c (BVar iVal (Just tyVal))
   watch tyCell $ \tyVal ->
     with iCell $ \iVal ->
-      write cell (BVar iVal (Just tyVal))
-  watch cell $ \(BVar iVal mTyVal) -> do
+      write c (BVar iVal (Just tyVal))
+  watch c $ \(BVar iVal mTyVal) -> do
     write iCell iVal
     maybe (return ()) (write tyCell) mTyVal
 
 fVar :: Cell s String -> Cell s Type -> Cell s Term -> ST s ()
-fVar sCell tyCell cell = do
+fVar sCell tyCell c = do
   watch sCell $ \sVal ->
     with tyCell $ \tyVal ->
-      write cell (FVar sVal (Just tyVal))
+      write c (FVar sVal (Just tyVal))
   watch tyCell $ \tyVal ->
     with sCell $ \sVal ->
-      write cell (FVar sVal (Just tyVal))
-  watch cell $ \(FVar sVal mTyVal) -> do
+      write c (FVar sVal (Just tyVal))
+  watch c $ \(FVar sVal mTyVal) -> do
     write sCell sVal
     maybe (return ()) (write tyCell) mTyVal
 
 abs :: Cell s Term -> Cell s Type -> Cell s Term -> ST s ()
-abs tmCell tyCell cell = do
+abs tmCell tyCell c = do
   watch tmCell $ \tmVal ->
     with tyCell $ \tyVal ->
-      write cell (Abs (Just tmVal) (Just tyVal))
+      write c (Abs (Just tmVal) (Just tyVal))
   watch tyCell $ \tyVal ->
     with tmCell $ \tmVal ->
-      write cell (Abs (Just tmVal) (Just tyVal))
-  watch cell $ \(Abs mTmVal mTyVal) -> do
+      write c (Abs (Just tmVal) (Just tyVal))
+  watch c $ \(Abs mTmVal mTyVal) -> do
     maybe (return ()) (write tmCell) mTmVal
     maybe (return ()) (write tyCell) mTyVal
 
 app :: Cell s Term -> Cell s Term -> Cell s Type -> Cell s Term -> ST s ()
-app t1Cell t2Cell tyCell cell = do
+app t1Cell t2Cell tyCell c = do
   watch t1Cell $ \t1Val ->
     with t2Cell $ \t2Val ->
       with tyCell $ \tyVal ->
-        write cell (App (Just t1Val) (Just t2Val) (Just tyVal))
+        write c (App (Just t1Val) (Just t2Val) (Just tyVal))
   watch t2Cell $ \t2Val ->
     with t1Cell $ \t1Val ->
       with tyCell $ \tyVal ->
-        write cell (App (Just t1Val) (Just t2Val) (Just tyVal))
+        write c (App (Just t1Val) (Just t2Val) (Just tyVal))
 
   watch tyCell $ \tyVal ->
     with t1Cell $ \t1Val ->
       with t2Cell $ \t2Val ->
-        write cell (App (Just t1Val) (Just t2Val) (Just tyVal))
+        write c (App (Just t1Val) (Just t2Val) (Just tyVal))
 
-  watch cell $ \(App mT1Val mT2Val mTyVal) -> do
+  watch c $ \(App mT1Val mT2Val mTyVal) -> do
     maybe (return ()) (write t1Cell) mT1Val
     maybe (return ()) (write t2Cell) mT2Val
     maybe (return ()) (write tyCell) mTyVal
@@ -79,59 +108,51 @@ data Type
   = TyFVar String
   | TyApp (Maybe Type) (Maybe Type)
   -- | Type
+  deriving Show
+
+instance Propagated Type where
+  merge (TyFVar s1) (TyFVar s2) =
+    TyFVar <$> merge s1 s2
+  merge (TyApp t11 t12) (TyApp t21 t22) =
+    TyApp <$> merge t11 t21 <*> merge t12 t22
+  merge _ _ = Contradiction mempty "type merge"
 
 tyFVar :: Cell s String -> Cell s Type -> ST s ()
-tyFVar sCell cell = do
+tyFVar sCell c = do
   watch sCell $ \sVal ->
-    write cell (TyFVar sVal)
-  watch cell $ \(TyFVar sVal) ->
+    write c (TyFVar sVal)
+  watch c $ \(TyFVar sVal) ->
     write sCell sVal
 
 
 tyApp :: Cell s Type -> Cell s Type -> Cell s Type -> ST s ()
-tyApp t1Cell t2Cell cell = do
+tyApp t1Cell t2Cell c = do
   watch t1Cell $ \t1Val ->
     with t2Cell $ \t2Val ->
-      write cell (TyApp (Just t1Val) (Just t2Val))
+      write c (TyApp (Just t1Val) (Just t2Val))
   watch t2Cell $ \t2Val ->
     with t1Cell $ \t1Val ->
-      write cell (TyApp (Just t1Val) (Just t2Val))
-  watch cell $ \(TyApp mT1Val mT2Val) -> do
+      write c (TyApp (Just t1Val) (Just t2Val))
+  watch c $ \(TyApp mT1Val mT2Val) -> do
     maybe (return ()) (write t1Cell) mT1Val
     maybe (return ()) (write t2Cell) mT2Val
 
--- just check whether term's TypeCell merges with the given TypeCell
-typecheck :: forall s. Cell s (Term, Type)
-          -> Cell s Type
-          -- XXX I don't think we should have another cell, just reject!
-          -> Cell s Bool
-          -> ST s ()
-typecheck tmCell tyCell result = do
-  maybeTm <- content tmCell
-  resultVal <- case maybeTm of
-    Just (_, tyCell') -> do
-      unify tyCell tyCell'
-      isJust <$> content tyCell
-    Nothing -> return False
-  write result resultVal
-
-mkTm :: Term -> ST s (Cell s (Term, Type))
-mkTm tm = do
-  tmCell <- known (tm, Nothing)
-  return tmCell
-
 main :: IO ()
 main =
-  putStrLn "here"
+  -- we want this typechecking to fail
+  print $ runST $ do
+    a <- cell
+    b <- cell
+    app <- cell
+    x <- cell
+    a' <- cell
 
---   -- we want this typechecking to fail
---   print $ runST $ do
---     a <- known $ TyFVar "a"
---     b <- known $ TyFVar "b"
---     app <- known $ TyApp a b
---     x <- mkTm $ FVar "x"
---     tm <- mkTm $ Annot app x
---     a' <- mkTm $ TyFVar "a"
---     result <- cell
---     typecheck tm a' result
---     content result
+    _ <- tyFVar <$> known "a" <*> pure a
+    _ <- tyFVar <$> known "b" <*> pure b
+    _ <- tyApp a b app
+    _ <- fVar <$> known "x" <*> pure app <*> pure x
+    _ <- tyFVar <$> known "a" <*> pure a'
+
+    unify a a'
+
+    (,) <$> content a <*> content a'
