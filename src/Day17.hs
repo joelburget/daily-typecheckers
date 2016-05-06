9ef8a0ebc8026ded821a31f02cc4a451fe2bf2fa
Joel Burget <joelburget@gmail.com>
2016-05-06 14:15:29 -0400

    Finish previous day's work.

--- src/Day17.hs
+++ src/Day17.hs
@@ -17,72 +17,28 @@ data Type
   deriving (Eq, Show)
 
 
--- bidirectionality: binders always carry a type
-data Term
-  = BVar Int                            -- infer
-  | FVar String Type                    -- infer
-  | Let Term Type Term                  -- check
+data CheckTerm
+  = Let InferTerm Type CheckTerm
+  | Neutral InferTerm
 
-  -- * fun intro / elim
-  | Abs Term Type                       -- check
-  | App Term Term                       -- infer
-
-  -- * new stuff
-  -- * record intro / elim
-  | Record [(String, Term)]             -- check
-  | AccessField Term String Type Term   -- infer
-
-  -- * variant intro / elim
-  | Variant String Term                 -- check
-  | Case Term [(String, Term)]          -- infer
+  -- "constructors"
+  | Abs CheckTerm
+  | Record [(String, CheckTerm)]
+  | Variant String CheckTerm
   deriving Show
 
--- t ^ u
---
--- Replace `BVar i j` with the j-th value from u, when the de Bruijn indices
--- match.
-open :: Term -> Term -> Term
-open = open' 0
 
--- { k -> u } t
-open' :: Int -> Term -> Term -> Term
-open' k u t = case t of
-  BVar i
-    | i == k -> u
-    | otherwise -> t
-  FVar _ _ -> t
-  Let t1 ty t2 -> Let (open' k u t1) ty (open' (k + 1) u t2)
+data InferTerm
+  = BVar Int
+  | FVar String Type
+  | Annot CheckTerm Type
 
-  Abs t' ty -> Abs (open' (k + 1) u t') ty
-  App t1 t2 -> App (open' k u t1) (open' k u t2)
+  -- eliminators
+  | App InferTerm CheckTerm
+  | AccessField InferTerm String Type CheckTerm
+  | Case InferTerm Type [(String, CheckTerm)]
+  deriving Show
 
-  Record fields -> Record (map (\(name, tm) -> (name, open' k u tm)) fields)
-  AccessField recTm name ty kTm ->
-    AccessField (open' k u recTm) name ty (open' (k + 1) u kTm)
-
-  Variant name t' -> Variant name (open' k u t')
-  Case varTm cases -> Case (open' k u varTm) (map (\(name, tm) -> (name, open' k u tm)) cases)
-
-close :: Term -> String -> Term
-close = close' 0
-
-close' :: Int -> Term -> String -> Term
-close' k t x = case t of
-  BVar _ -> t
-  FVar y _
-    | x == y -> BVar k
-    | otherwise -> t
-  Let t1 ty t2 -> Let (close' k t1 x) ty (close' (k + 1) t2 x)
-
-  Abs t' ty -> Abs (close' (k + 1) t' x) ty
-  App t1 t2 -> App (close' k t1 x) (close' k t2 x)
-
-  Record fields -> Record (map (\(name, tm) -> (name, close' k tm x)) fields)
-  AccessField recTm name ty kTm ->
-    AccessField (close' k recTm x) name ty (close' (k + 1) kTm x)
-
-  Variant name t' -> Variant name (close' k t' x)
-  Case varTm cases -> Case (close' k varTm x) (map (\(name, tm) -> (name, close' k tm x)) cases)
 
 type Ctx = [Type]
 type InCtx = EitherT String (Reader Ctx)
@@ -95,82 +51,176 @@ runChecker calc = case runReader (runEitherT calc) [] of
 unifyTy :: Type -> Type -> InCtx Type
 unifyTy (Function dom1 codom1) (Function dom2 codom2) =
   Function <$> unifyTy dom1 dom2 <*> unifyTy codom1 codom2
-unifyTy (RecordT left) (RecordT right) = do
-  -- take the intersection of possible records
-  -- TODO
-  return $ RecordT left
-unifyTy (VariantT left) (VariantT right) =
-  -- take the union of possible variants
-  -- overlap?
-  --
-  -- TODO
-  -- VariantT $ Map.union (Map.
-  return $ VariantT left
-unifyTy _ _ = left "failed unification"
+unifyTy (RecordT lTy) (RecordT rTy) = do
+  -- RecordT [(String, Type)]
+  -- Take the intersection of possible records. Make sure the overlap matches!
 
-check :: Term -> Type -> InCtx ()
+  let isect = Map.intersectionWith (,) (Map.fromList lTy) (Map.fromList rTy)
+  isect' <- mapM (uncurry unifyTy) isect
+
+  return $ RecordT (Map.toList isect')
+unifyTy (VariantT lTy) (VariantT rTy) = do
+  -- Take the union of possible variants. Make sure overlap matches!
+
+  let isect = Map.intersectionWith (,) (Map.fromList lTy) (Map.fromList rTy)
+  isect' <- mapM (uncurry unifyTy) isect
+
+  let union = Map.union (Map.fromList lTy) (Map.fromList rTy)
+
+  -- Overwrite with extra data we may have learned from unifying the types in
+  -- the intersection
+  let result = Map.union isect' union
+
+  return $ VariantT (Map.toList result)
+unifyTy l r = left ("failed unification " ++ show (l, r))
+
+check :: CheckTerm -> Type -> InCtx ()
 check tm ty = case tm of
+  -- t1: infered, t2: checked
   Let t1 ty' t2 -> do
-    check t1 ty'
-    t2Ty <- local (ty':) (infer t2)
-    _ <- unifyTy t2Ty ty
+    t1Ty <- infer t1
+    _ <- unifyTy t1Ty ty'
+    local (ty':) (check t2 ty)
+
+  Abs t' -> do
+    let Function domain codomain = ty
+    local (domain:) $ check t' codomain
+
+  Record fields -> do
+    -- Record [(String, CheckTerm)]
+    --
+    -- Here we define our notion of record subtyping -- we check that the
+    -- record we've been given has at least the fields expected of it and of
+    -- the right type.
+    let fieldMap = Map.fromList fields
+    case ty of
+      RecordT fieldTys -> mapM_
+        (\(name, subTy) -> do
+          case Map.lookup name fieldMap of
+            Just subTm -> check subTm subTy
+            Nothing -> left "failed to find required field in record"
+        )
+        fieldTys
+      _ -> left "failed to check a record against a non-record type"
+
+  Variant name t' -> do
+    -- Variant String CheckTerm
+    --
+    -- Here we define our notion of variant subtyping -- we check that the
+    -- variant we've been given is in the row
+    case ty of
+      VariantT fieldTys -> case Map.lookup name (Map.fromList fieldTys) of
+        Just expectedTy -> check t' expectedTy
+        Nothing -> left "failed to find required field in record"
+      _ -> left "failed to check a record against a non-record type"
+
+  Neutral tm' -> do
+    ty' <- infer tm'
+    _ <- unifyTy ty ty'
     return ()
 
-  -- XXX - bleh how to type the variable
-  Abs t' ty -> do
-    check t' ty
-    return ()
 
-  AccessField recTm name ty' kTm -> do
-    case recTm of
-      Record parts -> case Map.lookup name (Map.fromList parts) of
-        Just subTm -> do
-          check subTm ty'
-          rhsTy <- local (ty':) (infer kTm)
-          _ <- unifyTy rhsTy ty
-          return ()
-        Nothing -> left "didn't find the accessed key"
-      _ -> left "found non-record unexpectedly"
-
-  Case varTm cases -> do
-    varTmTy <- infer varTm
-    caseTy:caseTys <- local (varTmTy:) $
-      mapM (\(_, rhsTm) -> infer rhsTm) cases
-    rhsTy <- foldM unifyTy caseTy caseTys
-    _ <- unifyTy rhsTy ty
-    return ()
-
-  _ -> left $ "unexpected checked term: " ++ show tm
-
-infer :: Term -> InCtx Type
+infer :: InferTerm -> InCtx Type
 infer t = case t of
   BVar i -> (!! i) <$> ask
   FVar _ ty -> pure ty
+  Annot _ ty -> pure ty
 
   App t1 t2 -> do
-    -- t1Ty <- infer t1
     Function domain codomain <- infer t1
-    t2Ty <- infer t2
-
-    when (domain /= t2Ty) (left "app failed to type")
+    check t2 domain
     return codomain
 
-  Record fields -> do
-    fieldTys <- mapM (\(name, tm) -> (name,) <$> infer tm) fields
-    return (RecordT fieldTys)
-  -- kTm (recTm.name : ty)
+  -- k (rec.name : ty)
+  AccessField recTm name ty' kTm -> do
+    inspectTy <- infer recTm
+    case inspectTy of
+      RecordT parts -> case Map.lookup name (Map.fromList parts) of
+        Just subTy -> do
+          local (subTy:) (check kTm ty')
+          return ty'
+        Nothing -> left "didn't find the accessed key"
+      _ -> left "found non-record unexpectedly"
 
-  -- XXX bleh need subtyping here since we can only determine specific variant
-  -- this inhabits
-  Variant name t' -> do
-    t'Ty <- infer t'
-    return (VariantT [(name, t'Ty)])
+  Case varTm ty cases -> do
+    varTmTy <- infer varTm
+    case varTmTy of
+      VariantT tyParts -> do
+        let tyMap = Map.fromList tyParts
+            caseMap = Map.fromList cases
+            bothMatch = (Map.null (tyMap Map.\\ caseMap))
+                     && (Map.null (caseMap Map.\\ tyMap))
 
-  _ -> left "unexpected infered term"
+        when (not bothMatch) (left "case misalignment")
+
+        let mergedMap = Map.mergeWithKey
+              (\k a b -> Just (k, a, b))
+              (const (Map.fromList []))
+              (const (Map.fromList []))
+              tyMap
+              caseMap
+
+        mapM_
+          (\(_name, branchTy, rhs) -> local (branchTy:) (check rhs ty))
+          mergedMap
+      _ -> left "found non-variant in case"
+    return ty
+
 
 main :: IO ()
 main = do
+  let unit = Record []
+      unitTy = RecordT []
+      xy = Record
+        [ ("x", unit)
+        , ("y", unit)
+        ]
+      xyTy = RecordT
+        [ ("x", unitTy)
+        , ("y", unitTy)
+        ]
+
+  -- boring
   print $ runChecker $
-    let tm = Abs (BVar 0) ty
-        ty = Function (RecordT []) (RecordT [])
+    let tm = Abs (Neutral (BVar 0))
+        ty = Function unitTy unitTy
     in check tm ty
+
+  -- standard record
+  print $ runChecker $
+    let tm = Let
+          (Annot xy xyTy)
+          xyTy
+          (Neutral
+            (AccessField (BVar 0) "x" unitTy (Neutral (BVar 0)))
+          )
+    in check tm unitTy
+
+  -- record subtyping
+  print $ runChecker $
+    let xRecTy = RecordT [("x", unitTy)]
+        tm = Let
+          (Annot xy xRecTy)
+          xRecTy
+          (Neutral
+            (AccessField (BVar 0) "x" unitTy (Neutral (BVar 0)))
+          )
+    in check tm unitTy
+
+  -- variant subtyping
+  print $ runChecker $
+    let eitherTy = VariantT
+          [ ("left", unitTy)
+          , ("right", unitTy)
+          ]
+        tm = Let
+          (Annot (Variant "left" unit) eitherTy)
+          eitherTy
+          (Neutral
+            (Case (BVar 0) unitTy
+              [ ("left", (Neutral (BVar 0)))
+              , ("right", (Neutral (BVar 0)))
+              ]
+            )
+          )
+    in check tm unitTy
