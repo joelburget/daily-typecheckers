e5c3702df3c92cb764d05b250afc3094da9414ea
Joel Burget <joelburget@gmail.com>
2016-05-08 17:35:22 -0400

    Remove exotic HOAS term, add comments.

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,6 +1,7 @@
 -- Reflection, reification, evalation, normalization
 module Day19 where
 
+import Control.Arrow (right)
 import Control.Monad.Reader
 import Control.Monad.Gen
 import qualified Data.Map as Map
@@ -11,21 +12,25 @@ data Hoas
   | HApp Hoas Hoas
   | HBase String
 
-  -- exotic
-  | HGen Int
-
 -- An easily written nominal syntax
 data Nom
   = NLam String Nom
   | NApp Nom Nom
   | NVar String
   | NBase String
-  deriving Show
+
+instance Show Nom where
+  showsPrec p t = case t of
+    NVar str -> showString str
+    NBase str -> showString ("\"" ++ str ++ "\"")
+    NApp t1 t2 -> showParen (p >= 10) $
+      showsPrec 10 t1 . showString " " . showsPrec 10 t2
+    NLam name body -> showParen (p >= 10) $
+      showString ("\\" ++ name ++ " -> ") . showsPrec 10 body
 
 
 eval :: Hoas -> Hoas
 eval t = case t of
-  HGen _ -> error "unexpected HGen in eval"
   HBase _ -> t
   HApp (HLam _ f) x -> f x
   _ -> t
@@ -44,33 +49,43 @@ reflect' t = case t of
   NBase str -> return $ HBase str
 
 reify :: Hoas -> Nom
-reify t = runGen (runReaderT (reify' t) Map.empty)
+reify t = runGen (runReaderT (reify' (Right t)) Map.empty)
 
-reify' :: Hoas -> ReaderT (Map.Map Int Nom) (Gen Int) Nom
+reify' :: Either Int Hoas -> ReaderT (Map.Map Int Nom) (Gen Int) Nom
 reify' t = case t of
-  HLam name f -> do
-    ident <- gen
-    nom <- local (Map.insert ident (NVar name)) $ reify' $ f (HGen ident)
-    return $ NLam name nom
-  HGen ident -> (Map.! ident) <$> ask
-  HApp t1 t2 -> NApp <$> reify' t1 <*> reify' t2
-  HBase str -> return $ NBase str
+  Left ident -> (Map.! ident) <$> ask
+  Right t' -> case t' of
+    HLam name f -> do
+      ident <- gen
+      nom <- local (Map.insert ident (NVar name)) $
+        reify' $ (right f) (Left ident)
+      return $ NLam name nom
+    HApp t1 t2 -> NApp <$> reify' (Right t1) <*> reify' (Right t2)
+    HBase str -> return $ NBase str
 
 normalize :: Nom -> Nom
 normalize = reify . eval . reflect
 
 
+-- output:
+--
+-- > \f -> f
+-- \f -> f
+-- > (\x -> x) "xyz"
+-- "xyz"
+-- > (\x -> x) (\x -> x)
+-- \x -> x
 main :: IO ()
 main = do
   let nomTm = NLam "f" (NApp (NVar "f") (NBase "xyz"))
-  print (reify (reflect nomTm))
+  putStrLn $ "> " ++ show (reify (reflect nomTm))
   print (normalize nomTm)
 
   let nomId = NLam "x" (NVar "x")
   let nomTm2 = NApp nomId (NBase "xyz")
-  print (reify (reflect nomTm2))
+  putStrLn $ "> " ++ show (reify (reflect nomTm2))
   print (normalize nomTm2)
 
   let nomTm3 = NApp nomId nomId
-  print (reify (reflect nomTm3))
+  putStrLn $ "> " ++ show (reify (reflect nomTm3))
   print (normalize nomTm3)
